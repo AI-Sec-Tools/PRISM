ea57d76f437a52816bd4f9abe0ec24dd5f1163e9
Dewank Pant <dewankpant@gmail.com>
2023-04-05 09:25:00 -0700

    Add advanced risk engine with ML support and enhanced scoring

--- risk_engine.py
+++ risk_engine.py
@@ -40,3 +40,43 @@ class RiskScoringEngine:
             return "MEDIUM"
         else:
             return "LOW"
+
+# Enhanced Risk Scoring with ML Support
+class AdvancedRiskEngine(RiskScoringEngine):
+    def __init__(self):
+        super().__init__()
+        self.ml_enabled = False
+        
+    def enable_ml_scoring(self):
+        """Enable machine learning enhanced scoring."""
+        self.ml_enabled = True
+        self.logger.info("ML-enhanced scoring enabled")
+    
+    def calculate_enhanced_risk_score(self, vulnerability: Dict, context: Dict = None) -> Dict:
+        """Calculate enhanced risk score with context."""
+        base_score = self.calculate_risk_score(vulnerability)
+        
+        if context:
+            # Business impact multiplier
+            criticality = context.get('asset_criticality', 'medium')
+            multipliers = {'low': 0.8, 'medium': 1.0, 'high': 1.3, 'critical': 1.5}
+            business_multiplier = multipliers.get(criticality, 1.0)
+            
+            # Exposure multiplier
+            exposure = context.get('exposure_level', 'internal')
+            exposure_multipliers = {'internal': 1.0, 'external': 1.2, 'internet_facing': 1.4}
+            exposure_multiplier = exposure_multipliers.get(exposure, 1.0)
+            
+            enhanced_score = base_score * business_multiplier * exposure_multiplier
+        else:
+            enhanced_score = base_score
+        
+        return {
+            'base_score': base_score,
+            'enhanced_score': min(enhanced_score, 10.0),
+            'risk_level': self.categorize_risk(enhanced_score),
+            'factors': {
+                'business_impact': business_multiplier if context else 1.0,
+                'exposure_impact': exposure_multiplier if context else 1.0
+            }
+        }
